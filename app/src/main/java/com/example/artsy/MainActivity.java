34fb4d67e51bfd83778d8d00e4935ccce1660c6a
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-27 16:19:11 +0300

    refactor mainactivity using butterknife

--- app/src/main/java/com/example/artsy/MainActivity.java
+++ app/src/main/java/com/example/artsy/MainActivity.java
@@ -1,28 +1,32 @@
 package com.example.artsy;
 
-import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    private Button mWelcomeButton;
-    private TextView mArtsyTextView;
+    @BindView(R.id.welcomeButton) Button mWelcomeButton;
+    @BindView(R.id.artsyTextView) TextView mArtsyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mArtsyTextView = (TextView) findViewById(R.id.artsyTextView);
         Typeface adalgisaFont = Typeface.createFromAsset(getAssets(), "fonts/Adalgisa Personal Use.ttf");
         mArtsyTextView.setTypeface(adalgisaFont);
-        mWelcomeButton = (Button) findViewById(R.id.welcomeButton);
+
         mWelcomeButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
