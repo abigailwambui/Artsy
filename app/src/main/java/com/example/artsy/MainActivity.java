760edcdde917a0d64bff801217dae7a9ca19f3ff
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-27 15:10:43 +0300

    add a toast functionality to my button on mainactivity

--- app/src/main/java/com/example/artsy/MainActivity.java
+++ app/src/main/java/com/example/artsy/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.artsy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mWelcomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWelcomeButton = (Button) findViewById(R.id.welcomeButton);
+        mWelcomeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
