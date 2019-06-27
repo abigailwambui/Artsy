eda8ae9ca210c50a0d98138746670e5169c6502b
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-27 16:07:42 +0300

    add approprate dependancies

--- app/src/main/java/com/example/artsy/SignInActivity.java
+++ app/src/main/java/com/example/artsy/SignInActivity.java
@@ -2,13 +2,34 @@ package com.example.artsy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignInActivity extends AppCompatActivity {
+    private Button mSubmitArtsy;
+    private TextView mTextViewMain;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        mTextViewMain = (TextView) findViewById(R.id.textViewMain);
+        Typeface walkwayFont = Typeface.createFromAsset(getAssets(), "fonts/Walkway_Black.ttf");
+        mTextViewMain.setTypeface(walkwayFont);
+        mSubmitArtsy= (Button) findViewById(R.id.submitartsy);
+        mSubmitArtsy.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SignInActivity.this, "Welcome", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(SignInActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
