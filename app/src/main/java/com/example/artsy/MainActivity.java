c4a09d810b1db205d9b9e3e9f88ee6ca13c1515f
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-27 15:52:14 +0300

    implement downloaded fonts to my two activities

--- app/src/main/java/com/example/artsy/MainActivity.java
+++ app/src/main/java/com/example/artsy/MainActivity.java
@@ -3,19 +3,25 @@ package com.example.artsy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mWelcomeButton;
+    private TextView mArtsyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mArtsyTextView = (TextView) findViewById(R.id.artsyTextView);
+        Typeface adalgisaFont = Typeface.createFromAsset(getAssets(), "fonts/Adalgisa Personal Use.ttf");
+        mArtsyTextView.setTypeface(adalgisaFont);
         mWelcomeButton = (Button) findViewById(R.id.welcomeButton);
         mWelcomeButton.setOnClickListener(new View.OnClickListener(){
             @Override
