095e5690ea61439cd1e8bff889b03953b0d01583
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-27 16:22:54 +0300

    refactor signinactivity using butterknife

--- app/src/main/java/com/example/artsy/SignInActivity.java
+++ app/src/main/java/com/example/artsy/SignInActivity.java
@@ -1,23 +1,29 @@
 package com.example.artsy;
 
-import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SignInActivity extends AppCompatActivity {
-    private Button mSubmitArtsy;
-    private TextView mTextViewMain;
+    @BindView(R.id.textViewMain) TextView mTextViewMain;
+    @BindView(R.id.submitartsy) Button mSubmitArtsy;
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        ButterKnife.bind(this);
 
         mTextViewMain = (TextView) findViewById(R.id.textViewMain);
         Typeface walkwayFont = Typeface.createFromAsset(getAssets(), "fonts/Walkway_Black.ttf");
