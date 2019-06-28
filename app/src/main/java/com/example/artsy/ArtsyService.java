904ff71dc6edd1b5b436eb09af3b910b455fbb27
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-28 12:34:34 +0300

    add okhttp to service class to create and send our request

--- app/src/main/java/com/example/artsy/ArtsyService.java
+++ app/src/main/java/com/example/artsy/ArtsyService.java
@@ -1,10 +1,13 @@
 package com.example.artsy;
 
 import okhttp3.Callback;
+import okhttp3.OkHttpClient;
 
 public class ArtsyService {
 
     public static void findArts( Callback callback) {
 
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
     }
 }
