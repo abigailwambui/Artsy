52583bf6b70e470eae0c170e57598a4db63816cf
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-28 17:00:49 +0300

    add viewholder for my recyclerview

---
+++ app/src/main/java/com/example/artsy/adapters/ArtListAdapter.java
@@ -0,0 +1,46 @@
+package com.example.artsy.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.artsy.R;
+import com.example.artsy.models.Artsy;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ArtListAdapter extends RecyclerView.Adapter<ArtListAdapter.ArtViewHolder>{
+    public ArrayList<Artsy> mArtsies = new ArrayList<>();
+    private Context mContext;
+
+    public ArtListAdapter(Context context, ArrayList<Artsy> mArtsies) {
+        mContext = context;
+        mArtsies = artsies;
+    }
+
+    public class ArtViewHolder extends  RecyclerView.ViewHolder{
+        @BindView(R.id.artImageView)ImageView mArtImageView;
+        @BindView(R.id.artDescriptionTextView)TextView mDescriptionTextView;
+        @BindView(R.id.titleTextView) TextView mTitleTextView;
+        @BindView(R.id.cultureTextView) TextView mCultureTextView;
+
+        private Context mContext;
+
+        public ArtViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+            public void bindArt(Artsy art) {
+                mDescriptionTextView.setText(art.getDescription());
+                mTitleTextView.setText(art.getTitle());
+                mCultureTextView.setText(art.getCulture());
+            }
+        }
+    }
