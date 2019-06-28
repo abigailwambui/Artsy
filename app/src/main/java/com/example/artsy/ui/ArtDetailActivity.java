beae93506188d89a11ce0524f6a225167926cfe3
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-28 18:17:07 +0300

    add a viewpager to act as the container for our fragment

---
+++ app/src/main/java/com/example/artsy/ui/ArtDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.artsy.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArtDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art_detail);
+    }
+}
