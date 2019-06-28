f1b7995ca9568ef3408190a81015a7b5a6654972
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-28 13:03:46 +0300

    add an asynchronous request

--- app/src/main/java/com/example/artsy/ArtsyService.java
+++ app/src/main/java/com/example/artsy/ArtsyService.java
@@ -1,7 +1,10 @@
 package com.example.artsy;
 
+import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class ArtsyService {
 
@@ -9,5 +12,16 @@ public class ArtsyService {
 
         OkHttpClient client = new OkHttpClient.Builder()
                 .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.ARTSY_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.ARTSY_QUERY_PARAMETER, Constants.ARTSY_TOKEN);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+
     }
 }
