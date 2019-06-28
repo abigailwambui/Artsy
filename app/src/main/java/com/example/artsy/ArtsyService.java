59790df184bb01e933d5ffb7b03c2f56d7738e02
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-28 12:32:46 +0300

    place a artsyservice class that will access our api

---
+++ app/src/main/java/com/example/artsy/ArtsyService.java
@@ -0,0 +1,10 @@
+package com.example.artsy;
+
+import okhttp3.Callback;
+
+public class ArtsyService {
+
+    public static void findArts( Callback callback) {
+
+    }
+}
--- app/src/main/java/com/example/artsy/Constants.java
+++ app/src/main/java/com/example/artsy/Constants.java
@@ -2,4 +2,6 @@ package com.example.artsy;
 
 public class Constants {
     public static final String ARTSY_TOKEN = BuildConfig.ARTSY_TOKEN;
+    public static final String ARTSY_BASE_URL = "https://api.harvardartmuseums.org/object";
+    public static final String ARTSY_QUERY_PARAMETER = "apikey";
 }
