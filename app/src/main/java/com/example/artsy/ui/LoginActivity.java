2726c4379722a604e420df49e6d9cb2dfc22ed46
abigailwambui <abigailw15njuguna@gmail.com>
2019-07-05 14:28:04 +0300

    add a login activity

--- app/src/main/java/com/example/artsy/ui/LoginActivity.java
+++ app/src/main/java/com/example/artsy/ui/LoginActivity.java
@@ -3,13 +3,11 @@ package com.example.artsy.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.artsy.R;
-
-public class CreateAcountActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_acount);
+        setContentView(R.layout.activity_login);
     }
 }
