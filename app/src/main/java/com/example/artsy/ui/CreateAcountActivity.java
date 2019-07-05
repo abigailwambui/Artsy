811d369d31fcefe48dfe90c3d8c11f8a4d90da95
abigailwambui <abigailw15njuguna@gmail.com>
2019-07-05 14:19:42 +0300

    add an activity to create an account

---
+++ app/src/main/java/com/example/artsy/ui/CreateAcountActivity.java
@@ -0,0 +1,13 @@
+package com.example.artsy.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAcountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_acount);
+    }
+}
