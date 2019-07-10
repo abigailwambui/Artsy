35f4f14f9ecb7ceaebd72184f0a07d54d4f0859f
abigailwambui <abigailw15njuguna@gmail.com>
2019-07-10 12:36:18 +0300

    add a button to view saved arts

---
+++ app/src/main/java/com/example/artsy/SavedArtListActivity.java
@@ -0,0 +1,74 @@
+package com.example.artsy;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.artsy.adapters.FirebaseArtViewHolder;
+import com.example.artsy.models.Artsy;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SavedArtListActivity extends AppCompatActivity {
+    private DatabaseReference mArtsyReference;
+    private FirebaseRecyclerAdapter<Artsy, FirebaseArtViewHolder> mFirebaseAdapter;
+
+    @BindView(R.id.recyclerView)RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arts);
+        ButterKnife.bind(this);
+        mArtsyReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_ARTS);
+        setUpFirebaseAdapter();
+    }
+
+    private void setUpFirebaseAdapter(){
+        FirebaseRecyclerOptions<Artsy> options =
+                new FirebaseRecyclerOptions.Builder<Artsy>()
+                        .setQuery(mArtsyReference, Artsy.class)
+                        .build();
+
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Artsy, FirebaseArtViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FirebaseArtViewHolder firebaseRestaurantViewHolder, int position, @NonNull Restaurant restaurant) {
+                firebaseRestaurantViewHolder.bindArtsy(artsy);
+            }
+
+            @NonNull
+            @Override
+            public FirebaseArtViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.art_list_item, parent, false);
+                return new FirebaseArtViewHolder(view);
+            }
+        };
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mFirebaseAdapter!= null) {
+            mFirebaseAdapter.stopListening();
+        }
+    }
+}
--- app/src/main/java/com/example/artsy/adapters/FirebaseArtViewHolder.java
+++ app/src/main/java/com/example/artsy/adapters/FirebaseArtViewHolder.java
@@ -1,79 +1,79 @@
-//package com.example.artsy.adapters;
-//
-//import android.content.Context;
-//import android.content.Intent;
-//import android.support.v7.widget.RecyclerView;
-//import android.view.View;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import com.example.artsy.Constants;
-//import com.example.artsy.R;
-//import com.example.artsy.models.Artsy;
-//import com.example.artsy.ui.ArtDetailActivity;
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
-//import com.squareup.picasso.Picasso;
-//
-//import org.parceler.Parcels;
-//
-//import java.util.ArrayList;
-//
-//import butterknife.BindView;
-//
-//public class FirebaseArtViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-//    View mView;
-//    Context mContext;
-//
-//    public FirebaseArtViewHolder(View itemView) {
-//        super(itemView);
-//        mView = itemView;
-//        mContext = itemView.getContext();
-//        itemView.setOnClickListener(this);
-//    }
-//
-//    public void bindArtsy(mArtsies artsy) {
-//        ImageView artImageView = (ImageView) mView.findViewById(R.id.artImageView);
-//        TextView descriptionTextView = (TextView) mView.findViewById(R.id.artDescriptionTextView);
-//        TextView titleTextView = (TextView) mView.findViewById(R.id.titleTextView);
-//        TextView cultureTextView = (TextView) mView.findViewById(R.id.cultureTextView);
-//
-//
-//        Picasso.get().load(mArtsies.getPrimaryImageUrl()).into(mImageLabel);
-//
-//        descriptionTextView.setText(mArtsies.getDescription());
-//        titleTextView.setText(mArtsies.getTitle());
-//        cultureTextView.setText(mArtsies.getCulture());
-//            }
-//
-//    @Override
-//    public void onClick(View view) {
-//        final ArrayList<Artsy> mArtsies = new ArrayList<>();
-//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_ARTS);
-//        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-//
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-//                    mArtsies.add(snapshot.getValue(Artsy.class));
-//                }
-//
-//                int itemPosition = getLayoutPosition();
-//
-//                Intent intent = new Intent(mContext, ArtDetailActivity.class);
-//                intent.putExtra("position", itemPosition + "");
-//                intent.putExtra("restaurants", Parcels.wrap(mArtsies));
-//
-//                mContext.startActivity(intent);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//            }
-//        });
-//    }
-//}
-//}
+package com.example.artsy.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.artsy.Constants;
+import com.example.artsy.R;
+import com.example.artsy.models.Artsy;
+import com.example.artsy.ui.ArtDetailActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+
+public class FirebaseArtViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    View mView;
+    Context mContext;
+
+    public FirebaseArtViewHolder(View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+        itemView.setOnClickListener(this);
+    }
+
+    public void bindArtsy(Artsy artsy) {
+        ImageView artImageView = (ImageView) mView.findViewById(R.id.artImageView);
+        TextView descriptionTextView = (TextView) mView.findViewById(R.id.artDescriptionTextView);
+        TextView titleTextView = (TextView) mView.findViewById(R.id.titleTextView);
+        TextView cultureTextView = (TextView) mView.findViewById(R.id.cultureTextView);
+
+
+        Picasso.get().load(artsy.getPrimaryImageUrl()).into(mImageLabel);
+
+        descriptionTextView.setText(artsy.getDescription());
+        titleTextView.setText(artsy.getTitle());
+        cultureTextView.setText(artsy.getCulture());
+            }
+
+    @Override
+    public void onClick(View view) {
+        final ArrayList<Artsy> mArtsies = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_ARTS);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    mArtsies.add(snapshot.getValue(Artsy.class));
+                }
+
+                int itemPosition = getLayoutPosition();
+
+                Intent intent = new Intent(mContext, ArtDetailActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("restaurants", Parcels.wrap(mArtsies));
+
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+}
+}
