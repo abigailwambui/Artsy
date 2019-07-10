43935e877ff43c8d9aef2d347be62577ed199756
abigailwambui <abigailw15njuguna@gmail.com>
2019-07-10 10:07:29 +0300

    save node names to constants.java

--- app/src/main/java/com/example/artsy/Constants.java
+++ app/src/main/java/com/example/artsy/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String ARTSY_TOKEN = BuildConfig.ARTSY_TOKEN;
     public static final String ARTSY_BASE_URL = "https://api.harvardartmuseums.org/object";
     public static final String ARTSY_QUERY_PARAMETER = "apikey";
+    public static final String FIREBASE_CHILD_ARTS = "arts";
 }
